480fbea5c6cd7ec591728134506b27a6cdb2e803
Muneef <muneef@MacBook-Air-alkhas-b-Muneef.local>
2019-03-18 20:28:03 +0300

    Initial Commit

---
+++ RecordApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecordApp
+//
+//  Created by Muneef on 11/07/1440 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
